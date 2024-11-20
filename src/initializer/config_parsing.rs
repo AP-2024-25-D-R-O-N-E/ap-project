d996fe3766c469ccf3e7133bafe96b54fafb4759
ZGako <lhgino2003@gmail.com>
2024-11-20 01:50:24 +0100

    implemented external dependencies with no changes maintained functionality as before but now using external dependencies from both WGL repo and own group's drone repo

--- src/initializer/config_parsing.rs
+++ src/initializer/config_parsing.rs
@@ -1,35 +1,9 @@
 use std::fs;
 
-use serde::Deserialize;
-
-#[derive(Debug, Deserialize)]
-pub struct InitDrone {
-    pub id: u64,
-    pub connected_drone_ids: Vec<u64>,
-    pub pdr: f64,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct InitClient {
-    pub id: u64,
-    pub connected_drone_ids: Vec<u64>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct InitServer {
-    pub id: u64,
-    pub connected_drone_ids: Vec<u64>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct InitConfig {
-    pub drone: Vec<InitDrone>,
-    pub client: Vec<InitClient>,
-    pub server: Vec<InitServer>,
-}
+pub use wg_2024::config::Config as InitConfig;
 
 pub fn parse_config(config_path: String) -> InitConfig {
-    let config_data = fs::read_to_string("src/config.toml").expect("Unable to read config file");
+    let config_data = fs::read_to_string(config_path).expect("Unable to read config file");
 
     toml::from_str(&config_data).expect("Unable to parse TOML")
 }
--- src/routing/types.rs
+++
@@ -1,9 +0,0 @@
-
-pub type NodeId = u64;
-
-#[derive(Debug)]
-pub enum ServerType{
-    ChatServer, // only does chat
-    TextServer, // only does text
-    MediaServer, // does text and media
-}
