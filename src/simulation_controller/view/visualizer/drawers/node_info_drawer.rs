a17fe5e00d5047769b8519b539d2a1efedca28d3
Mattia Marini <marmattia26@gmail.com>
2025-01-18 12:10:48 +0100

    Added hide others button on node window

--- src/simulation_controller/view/visualizer/drawers/node_info_drawer.rs
+++ src/simulation_controller/view/visualizer/drawers/node_info_drawer.rs
@@ -5,7 +5,6 @@ use crate::simulation_controller::state::{DisplayOptions, State};
 
 pub fn draw_infos_for_selected_nodes(ctx: &Context, state: &mut State) {
     let selected_nodes = state.graph_section.g.selected_nodes().to_owned();
-    // println!("{:?}", selected_nodes);
     for node_index in selected_nodes {
         draw_node_info(ctx, node_index, state);
     }
@@ -19,34 +18,51 @@ pub fn draw_node_info(ctx: &Context, node_index: NodeIndex, state: &mut State) {
         .show(ctx, |ui| {
             ScrollArea::vertical().show(ui, |ui| {
                 ui.label(node_index.index().to_string());
-                if ui.button("Close").clicked() {
-                    // let new_selected_nodes = state
-                    //     .graph_section
-                    //     .g
-                    //     .selected_nodes()
-                    //     .to_owned()
-                    //     .into_iter()
-                    //     .filter(|n| *n != node_index)
-                    //     .collect();
-                    // println!("{:?}", new_selected_nodes);
-                    // state.graph_section.g.set_selected_nodes(new_selected_nodes);
-                    let n = state
-                        .graph_section
-                        .g
-                        .node_mut(node_index)
-                        .unwrap()
-                        .set_selected(false);
-                }
+                ui.horizontal(|ui| {
+                    if ui.button("Close").clicked() {
+                        // let new_selected_nodes = state
+                        //     .graph_section
+                        //     .g
+                        //     .selected_nodes()
+                        //     .to_owned()
+                        //     .into_iter()
+                        //     .filter(|n| *n != node_index)
+                        //     .collect();
+                        // println!("{:?}", new_selected_nodes);
+                        // state.graph_section.g.set_selected_nodes(new_selected_nodes);
+                        let n = state
+                            .graph_section
+                            .g
+                            .node_mut(node_index)
+                            .unwrap()
+                            .set_selected(false);
+                    }
+                    if ui.button("Hide others").clicked() {
+                        for selected_node_index in state.graph_section.g.selected_nodes().to_owned()
+                        {
+                            if selected_node_index != node_index {
+                                state
+                                    .graph_section
+                                    .g
+                                    .node_mut(selected_node_index)
+                                    .unwrap()
+                                    .set_selected(false);
+                            }
+                        }
+                    }
+                })
             });
 
             let events = state.events.get_events_list(DisplayOptions::ALL);
-            // println!("{:?}", events.len());
             CollapsingHeader::new("Logs").show(ui, |ui| {
-                for x in state.events.get_events_list(DisplayOptions::ALL) {
-                    ui.label(
-                        egui::RichText::new(format!("{:?}", x)).color(egui::Color32::LIGHT_GRAY),
-                    );
-                }
+                ScrollArea::vertical().show(ui, |ui| {
+                    for x in state.events.get_events_list(DisplayOptions::ALL) {
+                        ui.label(
+                            egui::RichText::new(format!("{:?}", x))
+                                .color(egui::Color32::LIGHT_GRAY),
+                        );
+                    }
+                })
             })
         });
 }
