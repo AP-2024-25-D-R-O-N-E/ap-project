a81cbc5241c621ef49f6d0347ed4ec1d6f1a339c
Luke48ita <luca.moretto-1@studenti.unitn.it>
2025-02-03 18:30:06 +0100

    Implemented receiver thread

--- src/client/client_test.rs
+++ src/client/client_test.rs
@@ -10,7 +10,7 @@ use petgraph::{
 };
 use wg_2024::{
     network::{NodeId, SourceRoutingHeader},
-    packet::{Ack, FloodRequest, FloodResponse, Fragment, Nack, NodeType, Packet, PacketType},
+    packet::{self, Ack, FloodRequest, FloodResponse, Fragment, Nack, NodeType, Packet, PacketType},
 };
 
 use crate::{
@@ -173,39 +173,96 @@ impl Client{
     }
 
 
+    fn sender_thread(
+        id: NodeId,
+        packet_s: Arc<RwLock<HashMap<u8, Sender<Packet>>>>,
+        scs: Sender<ClientEvent>,
+        fragment_r: Receiver<(NodeId, u64, Fragment)>,
+        nack_r: Receiver<Packet>,
+        condv: Condvar,
+        ack_packet_buffer: Arc<Mutex<HashMap<(u64, u64), Packet>>>,
+        topology: Arc<RwLock<GraphMap<NodeId, (), Undirected>>>,
+        topology_modified: Arc<Mutex<bool>>,
+        edge_nodes: Arc<RwLock<HashSet<NodeId>>>,
+    ) {
+        // records the routing table for the client (updated when an update to the topology is made)
+        let mut routing_table: HashMap<NodeId, Vec<NodeId>> = HashMap::new();
+
+        const MAX_OUTPUT_BUFFER: usize = 10;
+
+        loop {
+            select_biased!(
+                recv(nack_r) -> nack_res => {
+                    if let Ok(mut packet) = nack_res {
+                        // recalculate route if topology was modified
+                        let mut topology_mod_lock = topology_modified.lock().unwrap();
+
+                        let destination = packet.routing_header.hops.last().unwrap().clone();
+
+                        if *topology_mod_lock {
+                            Self::find_route(id, destination, &mut routing_table, topology.clone(), edge_nodes.clone());
+                            *topology_mod_lock = false;
+                        }
+
+                        packet.routing_header.hops = routing_table.get(&destination).unwrap().clone();
+
+                        let mut ack_buff = condv
+                        .wait_while(ack_packet_buffer.lock().unwrap(), |buff| {
+                            buff.len() + nack_r.len() >= MAX_OUTPUT_BUFFER
+                        })
+                        .unwrap();
+
+                        if let PacketType::MsgFragment(fragment) = &packet.pack_type {
+                            ack_buff.insert((packet.session_id, fragment.fragment_index), packet.clone());
+                        }
+
+                        Self::send_msg_packet(id, packet_s.clone(), scs.clone(), packet);
+
+                    }
+                },
+                recv(fragment_r) -> frag_res => {
+                    if let Ok((destination, session_id, fragment)) = frag_res {
+
+                        // receives normal packets
+
+                        // choose the route for the packet
+                        if routing_table.get(&destination).is_none() {
+                            // if the routing table doesn't have the next hop, then we need to update the routing table
+                            Self::find_route(id, destination, &mut routing_table, topology.clone(), edge_nodes.clone());
+                        }
+
+                        let fragment_index = fragment.fragment_index;
+
+                        let mut packet = Packet {
+                            routing_header: SourceRoutingHeader {
+                                hops: routing_table.get(&destination).unwrap().clone(),
+                                hop_index: 1
+                            },
+                            session_id,
+                            pack_type: PacketType::MsgFragment(fragment) };
+
+                        // get the ack_buffer through mutex and on condition
+                        let mut ack_buff = condv
+                        .wait_while(ack_packet_buffer.lock().unwrap(), |buff| {
+                            buff.len() + nack_r.len() >= MAX_OUTPUT_BUFFER
+                        })
+                        .unwrap();
+
+                        ack_buff.insert((session_id, fragment_index), packet.clone());
+
+                        Self::send_msg_packet(id, packet_s.clone(), scs.clone(), packet);
+                    }
+                }
+            );
+        }
+    }
+
+    fn command_handler_thread(id: NodeId){}
+
 }
 
-impl Client {
-    fn manage_nack(&self, nack: &Nack) {
-        //resend the packet
-        log::debug!(
-            "{} {} received a nack: {:?}",
-            "↳ client".green(),
-            self.id,
-            nack
-        );
-    }
-
-    fn manage_ack(&self, ack: &Ack) {
-        //free memory of message vector
-        log::debug!(
-            "{} {} received an ack: {:?}",
-            "↳ client".green(),
-            self.id,
-            ack
-        );
-    }
-
-    fn manage_msg_fragment(&self, msg: &Fragment) {
-        //call to the assembler
-        log::debug!(
-            "{} {} received a fragment: {:?}",
-            "↳ client".green(),
-            self.id,
-            msg
-        );
-    }
-
+//Thread: receiver
+impl Client{
     fn manage_flood_request(&self, mut packet: Packet) {
         log::debug!(
             "{} {} received a flood request: {:?}",
@@ -225,24 +282,22 @@ impl Client {
             // creates inverted route starting from path_trace
             let mut inverse_route: Vec<NodeId> =
                 new_flood_res.path_trace.iter().map(|(id, _)| *id).collect();
-            // ignore the rare occurrances where a loop might be created as its not computationally viable to consider it
             inverse_route.reverse();
 
             let packet = Packet {
                 pack_type: PacketType::FloodResponse(new_flood_res),
                 routing_header: SourceRoutingHeader {
                     hops: inverse_route,
-                    hop_index: 0,
+                    hop_index: 1,
                 },
                 session_id: 0, // it'll be whatever for now
             };
 
-            self.forward_packet(packet);
+            self.send_packet(packet);
         }
     }
 
-    fn manage_flood_response(&mut self, fr: &FloodResponse) {
-        //call to the assembler
+    fn manage_flood_response(&self, fr: FloodResponse) {
         log::debug!(
             "{} {} received a flood response: {:?}",
             "↳ client".green(),
@@ -252,33 +307,157 @@ impl Client {
 
         // check that the flood response is ours
         if fr.path_trace[0].0 == self.id {
-            let node = match fr.path_trace[0].1 {
-                NodeType::Drone => (fr.path_trace[0].0, HashableNodeType::Drone),
-                _ => (fr.path_trace[0].0, HashableNodeType::Edge),
+            let mut topology_lock = self.topology.write().unwrap();
+            let mut edge_nodes_lock = self.edge_nodes.write().unwrap();
+            let mut topology_mod_lock = self.topology_modified.lock().unwrap();
+
+            let mut current_index = topology_lock.add_node(self.id);
+
+            for (node_id, node_type) in fr.path_trace.iter().skip(1) {
+                let next_index = topology_lock.add_node(*node_id);
+                // add the edge nodes to the edge_nodes set
+                match node_type {
+                    NodeType::Client => {
+                        edge_nodes_lock.insert(*node_id);
+                    }
+                    NodeType::Drone => {}
+                    NodeType::Server => {
+                        edge_nodes_lock.insert(*node_id);
+                    }
+                }
+
+                topology_lock.add_edge(current_index, next_index, ());
+                current_index = next_index;
+            }
+            *topology_mod_lock = true;
+        }
+
+        log::info!("{} {:?}", "Client topology: ".green(), self.topology);
+    }
+
+
+    fn manage_msg_fragment(&self, packet: Packet, ready_send: Sender<(NodeId, u64)>) {
+        log::debug!(
+            "{} {} received a message fragment: {}",
+            "↳ client".green(),
+            self.id,
+            packet
+        );
+
+        let packet_source = packet.routing_header.hops[0];
+        let packet_msg_id = packet.session_id;
+
+        // inverse route calculation for the ack
+        let mut inverse_route = packet.routing_header.hops.clone();
+        inverse_route.reverse();
+
+        if let PacketType::MsgFragment(fragment) = packet.pack_type {
+            // send ack for this fragment
+            let packet = Packet {
+                pack_type: PacketType::Ack(Ack {
+                    fragment_index: fragment.fragment_index,
+                }),
+                routing_header: SourceRoutingHeader {
+                    hops: inverse_route,
+                    hop_index: 1,
+                },
+                session_id: packet_msg_id,
             };
 
-            let mut current_index = self.topology.add_node(node);
+            self.send_packet(packet);
 
-            for value in fr.path_trace.iter().skip(1) {
-                let node = match value.1 {
-                    NodeType::Drone => (value.0, HashableNodeType::Drone),
-                    _ => (value.0, HashableNodeType::Edge),
-                };
-                let new_node = self.topology.add_node(node);
-                self.topology.add_edge(current_index, new_node, ());
-                current_index = new_node;
+            // handle the fragment buffer and send the ready signal to the message handler thread
+            let mut fragment_buffer_lock = self.fragment_buffer.write().unwrap();
+
+            let total_frags = fragment.total_n_fragments;
+
+            if fragment_buffer_lock.contains_key(&(packet_source, packet_msg_id)) {
+                let mut frag_buffer = fragment_buffer_lock
+                    .get_mut(&(packet_source, packet_msg_id))
+                    .unwrap();
+
+                frag_buffer.push(fragment);
+
+                // if the buffer is full, tell the message handler thread to start assembling the fragments
+                if frag_buffer.len() == total_frags as usize {
+                    ready_send.send((packet_source, packet_msg_id));
+                }
+            } else {
+                fragment_buffer_lock.insert((packet_source, packet_msg_id), vec![fragment]);
+                // if the total frags is 1, then we can just send the message to the message handler thread
+                if total_frags == 1 {
+                    ready_send.send((packet_source, packet_msg_id));
+                }
+            }
+        }
+    }
+
+    fn manage_ack(&self, session_id: u64, ack: Ack) {
+        log::debug!(
+            "{} {} received an ack: {:?}",
+            "↳ client".green(),
+            self.id,
+            ack
+        );
+
+        let ack_key = (session_id, ack.fragment_index);
+
+        let mut ack_packet_buffer_lock = self.ack_packet_buffer.lock().unwrap();
+
+        if let Some(packet) = ack_packet_buffer_lock.remove(&ack_key) {
+            log::debug!(
+                "{} {} removed packet from ack buffer: {:?}",
+                "↳ client".green(),
+                self.id,
+                packet
+            );
+        }
+    }
+
+    fn manage_nack(&self, session_id: u64, nack: Nack, nack_send: Sender<Packet>) {
+        log::debug!(
+            "{} {} received a nack: {:?}",
+            "↳ client".green(),
+            self.id,
+            nack
+        );
+
+        // fast return flag to throw the packet away if a weird error happens
+        let mut fast_return = false;
+
+        match &nack.nack_type {
+            packet::NackType::ErrorInRouting(node) => {
+                self.topology.write().unwrap().remove_node(*node);
+                *self.topology_modified.lock().unwrap() = true;
+            }
+            packet::NackType::DestinationIsDrone => {
+                log::error!("Error: the destination is a drone");
+                fast_return = true;
+            }
+            packet::NackType::Dropped => {}
+            packet::NackType::UnexpectedRecipient(_) => {
+                log::error!("Error: the recipient is not the expected one");
+                fast_return = true;
             }
         }
 
-        log::info!("{} {:?}", "Client 1 topology: ".green(), self.topology);
+        let ack_key = (session_id, nack.fragment_index);
+
+        let mut ack_packet_buffer_lock = self.ack_packet_buffer.lock().unwrap();
+
+        if let Some(packet) = ack_packet_buffer_lock.remove(&ack_key) {
+            if fast_return {
+                return;
+            }
+            nack_send.send(packet);
+        } else {
+            log::error!("Error: the packet was not found in the ack buffer");
+        }
     }
 
-    fn forward_packet(&self, mut packet: Packet) {
-        packet.routing_header.hop_index += 1;
+    fn send_packet(&self, packet: Packet) {
         let next_node = packet.routing_header.hops[packet.routing_header.hop_index];
-
-        // finds the channel corresponding to the next node without any checks, since they were done previously
-        let send_channel = &self.ps[&next_node];
+        let send_channel = &self.packet_s.read().unwrap()[&next_node];
 
         log::debug!(
             "{} from {} - packet: {}",
@@ -290,17 +469,45 @@ impl Client {
         let res = send_channel.send(packet.clone());
 
         if let Err(mut packet) = res {
-            log::error!("The send inside channel gave an error, this shouldn't be happening");
+            log::error!("Error in the send inside channel");
         } else {
             self.scs.send(ClientEvent::PacketSent(packet));
         }
     }
 
+    fn initiate_flood(&mut self) {
+        for (id, sender) in self.packet_send.read().unwrap().iter() {
+            let packet = Packet {
+                pack_type: PacketType::FloodRequest(FloodRequest {
+                    path_trace: vec![(self.id, NodeType::Client)],
+                    flood_id: self.flood_id,
+                    initiator_id: self.id,
+                }),
+                routing_header: SourceRoutingHeader {
+                    hops: vec![],
+                    hop_index: 0,
+                },
+                session_id: 0,
+            };
+            self.flood_id += 1;
+
+            let res = sender.send(packet.clone());
+
+            if let Err(mut packet) = res {
+                log::error!("Error in the send inside channel");
+            } else {
+                self.scs.send(ClientEvent::PacketSent(packet));
+            }
+        }
+    }
+
     fn add_sender(&mut self, id: NodeId, sender: Sender<Packet>) {
-        self.ps.insert(id, sender);
+        self.packet_s.write().unwrap().insert(id, sender);
     }
 
     fn remove_channel(&mut self, id: NodeId) {
-        self.ps.remove(&id);
+        self.packet_s.write().unwrap().remove(&id);
     }
+
 }
+
